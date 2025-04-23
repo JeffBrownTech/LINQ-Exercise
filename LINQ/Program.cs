5cf3eb17f188ef117293a497481e2d101a407ac2
Jeff Brown <jeff@jeffbrown.tech>
2025-04-23 07:13:34 -0700

    Completed Linq Exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,16 @@
 ﻿namespace LINQ
+
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var westPlayoffTeams = new List<string> { "Thunder", "Grizzlies", "Nuggets", "Clippers", "Lakers", "Timberwolves", "Rockets", "Warriors"};
+
+            var orderByLength = westPlayoffTeams.OrderBy(x => x.Length).ToList();
+
+            Console.WriteLine("Teams ordered by string length:");
+            orderByLength.ForEach(x => Console.WriteLine(x));
         }
     }
 }
